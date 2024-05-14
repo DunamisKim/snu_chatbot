a353f9a58dfa4ebeabc6565bdf2f89b5fb4e83ed
DunamisKim <96373783+DunamisKim@users.noreply.github.com>
2024-05-14 16:20:19 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -127,7 +127,7 @@ def generate_response(query_text, vectorstore, callback):
         docs += f"'문서{i+1}':{doc.page_content}\n"
         
     # generator
-    llm = ChatOpenAI(model_name="gpt-4", temperature=0, streaming=True, callbacks=[callback])
+    llm = ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0, streaming=True, callbacks=[callback])
     
     # chaining
     rag_prompt = [
