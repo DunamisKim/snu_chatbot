7bb000c1d82a7f556275685ef5806f97ba343de0
DunamisKim <96373783+DunamisKim@users.noreply.github.com>
2024-05-14 16:15:22 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -22,6 +22,7 @@ class StreamHandler(BaseCallbackHandler):
         self.text += token
         self.container.markdown(self.text)
 
+
 # function to extract text from an HWP file
 import olefile
 import zlib
@@ -90,39 +91,67 @@ def get_pdf_text(filename):
 def process_uploaded_file(uploaded_file):
     # Load document if file is uploaded
     if uploaded_file is not None:
+        raw_text = ""
         # loader
-        # pdf파일을 처리하려면?
         if uploaded_file.type == 'application/pdf':
             raw_text = get_pdf_text(uploaded_file)
-        # hwp파일을 처리하려면? (hwp loader(parser)는 난이도 매우 어려움)
         elif uploaded_file.type == 'application/octet-stream':
             raw_text = get_hwp_text(uploaded_file)
-
+            
         # splitter
         text_splitter = CharacterTextSplitter(
-            separator = "\n\n",
+            # separator = "\r\n",
+            separator = "\n",
             chunk_size = 1000,
             chunk_overlap  = 200,
-            length_function = "문서에 내용이 나와있지 않은 내용은 답변을 하지마. 답변은 법률가 스타일로 친절하게 해줘."
+            length_function = len,
+            is_separator_regex = False,
+        )
+        all_splits = text_splitter.create_documents([raw_text])
+        
+        print("총 " + str(len(all_splits)) + "개의 passage")
+        
+        # storage
+        vectorstore = FAISS.from_documents(documents=all_splits, embedding=OpenAIEmbeddings())
+                
+        return vectorstore, raw_text
+    return None
+
+# generate response using RAG technic
+def generate_response(query_text, vectorstore, callback):
+
+    # retriever 
+    docs_list = vectorstore.similarity_search(query_text, k=3)
+    docs = ""
+    for i, doc in enumerate(docs_list):
+        docs += f"'문서{i+1}':{doc.page_content}\n"
+        
+    # generator
+    llm = ChatOpenAI(model_name="gpt-4", temperature=0, streaming=True, callbacks=[callback])
+    
+    # chaining
+    rag_prompt = [
+        SystemMessage(
+            content="너는 문서에 대해 질의응답을 하는 봇이야. 주어진 문서를 참고하여 사용자의 질문에 답변을 해줘. 문서에 내용이 정확하게 나와있지 않으면 대답하지 마."
         ),
         HumanMessage(
             content=f"질문:{query_text}\n\n{docs}"
         ),
     ]
-
-    response = llm(rag_prompt)
     
+    response = llm(rag_prompt)
     return response.content
 
 
 def generate_summarize(raw_text, callback):
-    # generator 
+
+    # generator
     llm = ChatOpenAI(model_name="gpt-4-1106-preview", temperature=0, streaming=True, callbacks=[callback])
     
     # prompt formatting
     rag_prompt = [
         SystemMessage(
-            content= "다음 나올 문서를 법률가 스타일로 요약해줘."
+            content="다음 나올 문서를 법률가 스타일로 요약해줘. 중요한 내용만."
         ),
         HumanMessage(
             content=raw_text
@@ -134,8 +163,8 @@ def generate_summarize(raw_text, callback):
 
 
 # page title
-st.set_page_config(page_title='🦜🔗 공무집행을 도와주는 Law-bot')
-st.title('🦜🔗 공무집행을 도와주는 Law-bot')
+st.set_page_config(page_title='공무집행을 도와주는 Public-Lawbot')
+st.title('공무집행을 도와주는 Public-Lawbot')
 
 import os
 api_key = st.sidebar.text_input("Enter your OpenAI API Key", type="password")
@@ -158,7 +187,7 @@ if uploaded_file:
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         ChatMessage(
-            role="assistant", content="안녕하세요! 저는 공무원의 업무 집행을 도와주는 챗봇입니다. 어떤게 궁금하신가요?"
+            role="assistant", content="안녕하세요! 저는 공무 집행을 이해를 도와주는 챗봇입니다. 어떤게 궁금하신가요?"
         )
     ]
 
@@ -167,7 +196,7 @@ for msg in st.session_state.messages:
     st.chat_message(msg.role).write(msg.content)
     
 # message interaction
-if prompt := st.chat_input("규정에 대하여 궁금한 내용을 자유롭게 입력해보세요!"):
+if prompt := st.chat_input("'요약'이라고 입력해보세요!"):
     st.session_state.messages.append(ChatMessage(role="user", content=prompt))
     st.chat_message("user").write(prompt)
 
