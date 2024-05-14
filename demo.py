4de95c1320510c2bac60e855e2c17dab9f377b7e
DunamisKim <96373783+DunamisKim@users.noreply.github.com>
2024-05-14 15:25:52 +0900

    Update demo.py

--- demo.py
+++ demo.py
@@ -150,7 +150,7 @@ def generate_summarize(raw_text, callback):
     # prompt formatting
     rag_prompt = [
         SystemMessage(
-            content="다음 나올 문서를 'Notion style'로 요약해줘. 중요한 내용만."
+            content="다음 나올 문서를 '래퍼 스타일'로 요약해줘. 중요한 내용만."
         ),
         HumanMessage(
             content=raw_text
